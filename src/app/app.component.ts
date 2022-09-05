1e9c4656f01930d0f14c2a1c53543bb9dfb50c52
Nima <Nima.aghdaei@gmail.com>
2022-09-05 13:15:12 +0430

    all comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'newtype';
+  randomtext = faker.lorem.sentence();
+  onchangetext(text:string){
+    console.log(text);
+    
+
+  }
 }
